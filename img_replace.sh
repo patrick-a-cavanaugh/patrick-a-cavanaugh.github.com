4500d7a15d4228d623ee78795b047af5348e0e8a
Patrick Cavanaugh <me@patcavanaugh.info>
2021-10-02 11:35:01 +0000

    post 2021-10-02 with images

--- img_replace.sh
+++ img_replace.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-DATE="2021-09-30"
-sed -i -r 's,\[(IMG_[0-9]{4})],<div class="img">\n\t<a href="/assets/images/'$DATE'/\1.jpg" title="Click to view full size"><img src="/assets/images/'$DATE'/\1.jpg" width="580" alt="" /></a>\n</div>\n,g'  $1
-sed -i -r 's,\[(IMG_[0-9]{4})\|(.+?)],<div class="img img-caption">\n\t<a href="/assets/images/'$DATE'/\1.jpg" title="Click to view full size"><img src="/assets/images/'$DATE'/\1.jpg" width="580" alt="" /></a>\n\t<p>\2</p>\n</div>\n,g'  $1
+DATE="2021-10-01"
+sed -i -r 's,\[(IMG_[0-9]{4})],<div class="img">\n\t<a target="_blank" href="/assets/images/'$DATE'/\1.jpg" title="Click to view full size"><img src="/assets/images/'$DATE'/\1.jpg" width="580" alt="" /></a>\n</div>\n,g'  $1
+sed -i -r 's,\[(IMG_[0-9]{4})\|(.+?)],<div class="img img-caption">\n\t<a target="_blank" href="/assets/images/'$DATE'/\1.jpg" title="Click to view full size"><img src="/assets/images/'$DATE'/\1.jpg" width="580" alt="" /></a>\n\t<p>\2</p>\n</div>\n,g'  $1
 # :%s/\[\(IMG_[0-9]+\)\]/<div class="image">\r<img src="\1" width="580" alt="" \/>\r<\/div>/g
